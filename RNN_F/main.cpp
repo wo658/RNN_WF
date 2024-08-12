d932f19efa91691e70a9c78fdd5e86adcd8da619
KJH1128 <whdgns2347@naver.com>
2024-08-12 23:09:14 +0900

    update

--- RNN_F/main.cpp
+++ RNN_F/main.cpp
@@ -22,8 +22,8 @@ std::vector<std::vector<std::vector<double>>> split2DVector(const std::vector<st
 int main() {
 	//Regression 
 	//Data Load
-	string dataPath = "C:\\Users\\ecmdev\\Desktop\\123\\MLP_Ex\\MLP_Ex\\ProcessDifference_train.csv";
-	string dataPath_test = "C:\\Users\\ecmdev\\Desktop\\123\\MLP_Ex\\MLP_Ex\\ProcessDifference_test.csv";
+	string dataPath = "C:\\Users\\whdgn\\Downloads\\RNN_WF-master\\RNN_WF-master\\RNN_PY\\ProcessDifference_train.csv";
+	string dataPath_test = "C:\\Users\\whdgn\\Downloads\\RNN_WF-master\\RNN_WF-master\\RNN_PY\\ProcessDifference_test.csv";
 	const char* NameofData = dataPath.c_str();
 	const char* testData = dataPath.c_str();
 	vector<vector<double>> train;
@@ -62,48 +62,72 @@ int main() {
 		}
 		// 선택사항 1 y 정규화
 		// 1 . 회귀 문제의 경우 타겟값이 넓은 범위에 걸쳐 있을 때 정규화
-		//y_train[i][0] = (y_train[i][0] - min_y) / (max_y - min_y);
+		y_train[i][0] = (y_train[i][0] - min_y) / (max_y - min_y);
 	}
 	int i = x_train[0].size();  // Number of input nodes
 	int h = 30;  // Number of hidden nodes
 	int o = 1;  // Number of output nodes
 
-	RNN rnn(i, h, o, 0.01);
+	RNN rnn(i, h, o, 0.001);
 	double error = 0;
 
-	int b_size = 100;
+	int b_size = 50;
 	vector<vector<double>> x_train_temp, y_train_temp;
 
 	// 벡터를 자를 행 크기 설정
-	size_t chunkSize = 100;
+	size_t chunkSize = 50;
 	// 2차원 벡터를 나눕니다.
 	std::vector<std::vector<std::vector<double>>> splitVecs_x = split2DVector(x_train, chunkSize);
 	std::vector<std::vector<std::vector<double>>> splitVecs_y = split2DVector(y_train, chunkSize);
  
 	for (int epoch = 0; epoch < 1000; epoch++) {
 
-
+		
 		for (int i = 0; i < splitVecs_x.size(); i++) {
-			std::vector<std::vector<double>> sequenceOutputs = rnn.forward(splitVecs_x[i]);
+
 			int j = 0;
-			for (int t = splitVecs_x[i].size() * i; t < (i+1) *splitVecs_x[i].size(); t++) {
+				std::vector<std::vector<double>> sequenceOutputs = rnn.forward(splitVecs_x[i]);
+			for (int t = splitVecs_x[i].size() * i; t < (i + 1) * splitVecs_x[i].size(); t++)
 				error += (sequenceOutputs[j][0] - y_train[t][0]) * (sequenceOutputs[j][0] - y_train[t][0]);
 				j++;
 				//cout << "t is = " <<t  << " "<< endl;
 
-				rnn.backward(splitVecs_x[i], splitVecs_y[i], 1);
-			}
-			error = error / (double)x_train.size();
-			std::cout << "Epoch : " << epoch << " Error : " << error << std::endl;
+				rnn.backward(splitVecs_x[i], splitVecs_y[i],sequenceOutputs,1);
+				
+			
+			
 
 			
 
 
 
 
-			error = 0;
 		}
-		
+		error = error / x_train.size();
+		std::cout << "Epoch : " << epoch << " Error : " << error << std::endl;
+
+		error = 0;
+		 /*
+			int j = 0;
+			
+			std::vector<std::vector<double>> sequenceOutputs = rnn.forward(x_train);
+			for (int t = 0; t <x_train.size(); t++) 
+				error += (sequenceOutputs[j][0] - y_train[t][0]) * (sequenceOutputs[j][0] - y_train[t][0]);
+			j++;
+			//cout << "t is = " <<t  << " "<< endl;
+
+			rnn.backward(x_train, y_train, sequenceOutputs, 1);
+			
+			error = error / x_train.size();
+			std::cout << "Epoch : " << epoch << " Error : " << error << std::endl;
+
+
+
+
+
+
+			error = 0;
+		*/
 	}
 
 	return 0;
